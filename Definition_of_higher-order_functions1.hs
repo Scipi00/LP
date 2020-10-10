dd4259a4a5e733f2c055f1fffd2b636f09752d95
Scipi00 <nicod3m00@gmail.com>
2020-10-10 14:09:47 +0200

    definition of higher order funcions 1 done 2 in progress

--- Definition_of_higher-order_functions1.hs
+++ Definition_of_higher-order_functions1.hs
@@ -1,10 +1,39 @@
 myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl f x [] = x
+myFoldl f x (y:ys) = myFoldl f (f x y) ys
+
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
+myFoldr f x [] = x
+myFoldr f x (y:ys) = f y (myFoldr f x ys)
+
 myIterate :: (a -> a) -> a -> [a]
+myIterate f x = [x] ++ myIterate f (f x)
+
 myUntil :: (a -> Bool) -> (a -> a) -> a -> a
+myUntil g f x = if g x then x else myUntil g f (f x)
+
 myMap :: (a -> b) -> [a] -> [b]
+myMap f [] = []
+myMap f (x:xs) = [f x] ++ myMap f xs
+
 myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter g [] = []
+myFilter g (x:xs) = if g x then [x] ++ myFilter g xs else myFilter g xs
+
 myAll :: (a -> Bool) -> [a] -> Bool
+myAll g [] = True
+myAll g (x:xs) = if g x then myAll g xs else False
+
 myAny :: (a -> Bool) -> [a] -> Bool
+myAny g [] = False
+myAny g (x:xs) = if g x then True else myAny g xs
+
 myZip :: [a] -> [b] -> [(a, b)]
+myZip [] _ = []
+myZip _ [] = []
+myZip (x:xs) (y:ys) = [(x,y)] ++ myZip xs ys
+
 myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith _ [] _ = []
+myZipWith _ _ [] = []
+myZipWith f (x:xs) (y:ys) = [f x y] ++ myZipWith f xs ys
--- Definition_of_higher-order_functions2.hs
+++ Definition_of_higher-order_functions2.hs
@@ -1,6 +1,48 @@
-Define a function countIf :: (Int -> Bool) -> [Int] -> Int that, given a predicate on integers and a list of integers, returns the number of elements in the list that satify the predicate.
-Define a function pam :: [Int] -> [Int -> Int] -> [[Int]] that, given a list of integers and a list of functions from integers to integers, returns the list consisting if applying each of the functions in the second list to the elements in the first list.
-Define a function pam2 :: [Int] -> [Int -> Int] -> [[Int]] that, given a list of integers and a list of functions from integers to integers, returns the list of lists where each list if the result of applying, one after the other, the function in the second list to each element in the first list.
-Define a function filterFoldl :: (Int -> Bool) -> (Int -> Int -> Int) -> Int -> [Int] -> Int that returns a fold of all the elements that satisfy the given predicate.
-Define a function insert :: (Int -> Int -> Bool) -> [Int] -> Int -> [Int] that, given a relation between integers, a list and un element, return the list with the inserted element according to the relation.
-Use function insert, in order to define function insertionSort :: (Int -> Int -> Bool) -> [Int] -> [Int] that orders a list according to the given relation.
+--Auxiliary stuff
+myLength :: [Int] -> Int
+myLength [] = 0
+myLength (x:xs) = 1 + myLength(xs)
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter g [] = []
+myFilter g (x:xs) = if g x then [x] ++ myFilter g xs else myFilter g xs
+
+{-Define a function countIf :: (Int -> Bool) -> [Int] -> Int that, given a predicate on integers and a list of integers, returns the number of elements in the list that satify the predicate.-}
+
+countIf :: (Int -> Bool) -> [Int] -> Int
+countIf g [] = 0
+countIf g (x:xs) = if g x then 1 + countIf g xs else countIf g xs
+
+--Auxiliary stuff
+myMap :: (a -> b) -> [a] -> [b]
+myMap f [] = []
+myMap f (x:xs) = [f x] ++ myMap f xs
+
+{-Define a function pam :: [Int] -> [Int -> Int] -> [[Int]] that, given a list of integers and a list of functions from integers to integers, returns the list consisting if applying each of the functions in the second list to the elements in the first list.-}
+
+pam :: [Int] -> [Int -> Int] -> [[Int]]
+pam = 
+--Auxiliary stuff
+
+{-Define a function pam2 :: [Int] -> [Int -> Int] -> [[Int]] that, given a list of integers and a list of functions from integers to integers, returns the list of lists where each list if the result of applying, one after the other, the function in the second list to each element in the first list.-}
+
+
+
+--Auxiliary stuff
+
+{-Define a function filterFoldl :: (Int -> Bool) -> (Int -> Int -> Int) -> Int -> [Int] -> Int that returns a fold of all the elements that satisfy the given predicate.-}
+
+
+
+--Auxiliary stuff
+
+{-Define a function insert :: (Int -> Int -> Bool) -> [Int] -> Int -> [Int] that, given a relation between integers, a list and un element, return the list with the inserted element according to the relation.-}
+
+
+
+--Auxiliary stuff
+
+{-Use function insert, in order to define function insertionSort :: (Int -> Int -> Bool) -> [Int] -> [Int] that orders a list according to the given relation.-}
+
+
+
